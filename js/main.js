9b24bd4fdad423e60d8bcbb4abfcb1cfad5b096f
helen58042 <helen58042@gmail.com>
2022-02-28 00:56:11 +0700

    new slider

--- js/main.js
+++ js/main.js
@@ -1,89 +1,26 @@
 $(function(){
-	// Кнопка "Вверх"
-	var btn = $('.btn-scroll-up');
+    // OWL-CAROUSEL
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: false,
+        dots: true,
+        navText: ['&#8678;', '&#8680;'],
+        // autoplay: true,
+        autoplayTimeout: 6000,
+        animateOut: 'fadeOut',
+        autoplayHoverPause: true,
+        responsive: {
+            767: {
+                nav: true
+            }
+        }
+    });
 
-	$(window).scroll(function() {
-		if ($(window).scrollTop() > 300) {
-			btn.addClass('show');
-		} else {
-			btn.removeClass('show');
-		}
-	});
-
-	btn.on('click', function(e) {
-		e.preventDefault();
-		$('html, body').animate({scrollTop: 0}, '300');
-	});
-
-	// Кнопка-гамбургер
-	$('.menu-open').click(function(e) {
-		e.preventDefault();
-		$('ul.menu').toggleClass('show');
-	});
-
-	$('ul.menu li a').click(function(e) {  // при клике на ссылку закрыть меню
-		e.preventDefault();
-		$('ul.menu-open').toggleClass('show');
-	});
-
-	// Закладки на странице "О стране"
-	$(".about-form").on("click", ".about-tab", function() {
-		// console.log($(this).index());
-		$(".about-form .about-tab").removeClass("about-tab__active");
-		$(this).addClass("about-tab__active");
-	});
-
-
-
-	// Плавная прокрутка
-	// 30 минут 30 секунд
-
-
-
-
-//	$('.go_to').click( function(){
-//		var scroll_el = $(this).attr('href');
-//		if ($(scroll_el).length != 0) {
-//			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1500);
-//		}
-//		return false;
-//	});
-
-
-
-
-
-	// $('.gallery-slider__row').slick({
-	// 	centerMode: true,
-	// 	centerPadding: '60px',
-	// 	slidesToShow: 5,
-	// 	responsive: [
-	// 		{
-	// 			breakpoint: 768,
-	// 			settings: {
-	// 				arrows: false,
-	// 				centerMode: true,
-	// 				centerPadding: '40px',
-	// 				slidesToShow: 3
-	// 			}
-	// 		},
-	// 		{
-	// 			breakpoint: 480,
-	// 			settings: {
-	// 				arrows: false,
-	// 				centerMode: true,
-	// 				centerPadding: '40px',
-	// 				slidesToShow: 1
-	// 			}
-	// 		}
-	// 	]
-	// });
-
-
-//	при клике оставаться на том же месте    ?????????
-//	$('.car-slider__href').click(function(e) {
-//		e.preventDefault();
-//	});
-
+    // BURGER
+    $('.header__burger').click(function(event) {
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
 });
 
